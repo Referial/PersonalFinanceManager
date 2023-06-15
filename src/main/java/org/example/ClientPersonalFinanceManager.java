0e733e1fc9749a86c6d6acad66041949529d78e0
Nikita Mayorov <MayorovNikita7@yandex.ru>
2023-06-15 12:48:22 +0500

    фиксация 15.06.2023__12:48

--- src/main/java/org/example/ClientPersonalFinanceManager.java
+++ src/main/java/org/example/ClientPersonalFinanceManager.java
@@ -13,80 +13,48 @@ import static org.example.ServerSocket.HOST;
 import static org.example.ServerSocket.PORT;
 
 public class ClientPersonalFinanceManager {
-    protected static int cycle = 0;
-
     public static void main(String[] args) {
-
-        AuxiliaryСlasses auxiliaryСlasses = new AuxiliaryСlasses();
 
         Scanner scanner = new Scanner(System.in);
 
         try (Socket socket = new Socket(HOST, PORT);
              PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
              BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-            System.out.println("Введите имя пользователя:");
 
-            writer.println(scanner.nextLine());
-            System.out.println(reader.readLine());
+            JSONObject file = new JSONObject();
 
-            int x = 0;
+            String part1;
+            String part2;
+            String part3;
 
-            while (x == 0) {
+            System.out.println("Введите через пробел: Название продукта, дата покупки в формате - год.месяц.число " +
+                    "и сумму покупки.");
+            String line = scanner.nextLine();
+            String[] parts = line.split(" ");
 
-                System.out.println("Выберите: \n" + "1. Добавить покупку. \n" + "2. Посмотреть наибольшите расходы. \n"
-                        + "3. Выйти из приложения.");
-                int action = scanner.nextInt();
-                String ac = Integer.toString(action);
-                writer.println(ac);
+            if (parts.length == 3) {
+                part1 = parts[0]; //Название товара
+                part2 = parts[1]; //Дата
+                part3 = parts[2]; //Сумма
 
-                switch (action) {
-                    case 1:
-                        JSONObject file = new JSONObject();
+                boolean chek = AuxiliaryСlasses.checkingTheEnteredData(part2, part3, parts);
 
-                        String part1;
-                        String part2;
-                        String part3;
+                if (chek == true) {
+                    file.put("title", part1);
+                    file.put("date", part2);
+                    file.put("sum", part3);
 
-                        System.out.println("Введите через пробел: Название продукта, дата покупки в формате - год.месяц.число " +
-                                "и сумму покупки.");
-                        String line = scanner.nextLine();
-                        String[] parts = line.split(" ");
+                    String strJson = file.toString();
 
-                        if (parts.length == 3) {
-                            part1 = parts[0]; //Название товара
-                            part2 = parts[1]; //Дата
-                            part3 = parts[2]; //Сумма
-
-                            boolean chek = AuxiliaryСlasses.checkingTheEnteredData(part2, part3, parts);
-
-                            if (chek == true) {
-                                file.put("title", part1);
-                                file.put("date", part2);
-                                file.put("sum", part3);
-
-                                String strJson = file.toString();
-
-                                writer.println(String.format(strJson));
-                            }
-                        } else {
-                            System.out.println("Некоректный ввод.");
-                        }
-                        break;
-                    case 2:
-                        System.out.println(reader.readLine());
-                        System.out.println(reader.readLine());
-                        System.out.println(reader.readLine());
-                        System.out.println(reader.readLine());
-                        break;
-                    case 3:
-                        System.out.println("Сервер закончил работу.");
-                        x++;
-                        break;
-                    default:
-                        System.out.println(reader.readLine());
-                        break;
+                    writer.println(String.format(strJson));
                 }
+            } else {
+                System.out.println("Некоректный ввод.");
             }
+            System.out.println(reader.readLine());
+            System.out.println(reader.readLine());
+            System.out.println(reader.readLine());
+            System.out.println(reader.readLine());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,46 +13,30 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         try (java.net.ServerSocket server = new java.net.ServerSocket(org.example.ServerSocket.PORT)) {
-            System.out.println("Сервер производит запуск: \n" + "Loading…  ██████████ 100%\n" + "Сервер запущен.");
+            System.out.println("Сервер запущен.");
 
             AuxiliaryСlasses auxiliaryСlasses = new AuxiliaryСlasses();
             AuxiliaryСlasses.parsingTheDataTable();
 
-            try (Socket socket = server.accept(); BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
 
-                final String name = in.readLine();
-                System.out.println("Зарегестрировался пользователь: " + name);
-                out.println(String.format("Привет %s, твой порт %d", name, socket.getPort()));
+            while (true) {
 
-                int x = 0;
+                try (Socket socket = server.accept();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
 
-                while (x == 0) {
-                    int actionInt = Integer.parseInt(in.readLine());
+                    String LineJson = in.readLine();
 
-                    switch (actionInt) {
-                        case 1:
-                            String LineJson = in.readLine();
+                    ObjectMapper mapper = new ObjectMapper();
+                    Map<String, String> map = mapper.readValue(LineJson, Map.class);
 
-                            ObjectMapper mapper = new ObjectMapper();
-                            Map<String, String> map = mapper.readValue(LineJson, Map.class);
+                    String product = map.get("title");
+                    String date = map.get("date");
+                    Integer sum = Integer.parseInt(map.get("sum"));
 
-                            String product = map.get("title");
-                            String date = map.get("date");
-                            Integer sum = Integer.parseInt(map.get("sum"));
+                    auxiliaryСlasses.addingToTheShoppingList(product, date, sum);
 
-                            auxiliaryСlasses.addingToTheShoppingList(product, date, sum);
-                            break;
-                        case 2:
-                            out.println(auxiliaryСlasses.arrayСategory());
-                            break;
-                        case 3:
-                            System.out.println("Сервер закончил свою работу.");
-                            x++;
-                            break;
-                        default:
-                            out.println("Некоректный ввод.");
-                            break;
-                    }
+                    out.println(auxiliaryСlasses.arrayСategory());
                 }
             }
         } catch (IOException e) {
